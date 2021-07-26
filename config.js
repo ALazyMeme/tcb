565cb9a0324ce1b466a077f7912f5fb4232ece40
ALazyMeme <jkeasley@icloud.com>
2021-07-26 21:13:39 +1000

    Add weest

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ const opts = {
     secure: true,
   },
   identity: {
-    username: "dankfeelsbot",
+    username: "roboweest",
     password: secrets.ircPassword,
   },
   channels: [
-    "#alazymeme",
-    "#dankfeelsbot"
+    "#weest",
+    "#roboweest"
   ],
 };
 
@@ -27,15 +27,15 @@ const krakenClientId = secrets.krakenClientId;
 let administrators = ["alazymeme"];
 
 // The bot will post a "I am running"-style message to this channel on startup.
-const startupChannel = "alazymeme";
+const startupChannel = "weest";
 
 // if a channel is offline-only protected, and a change occurs, the bot prints
 // to this channel instead of the channel the change occurred in.
-const onlinePrintChannel = "dankfeelsbot";
+const onlinePrintChannel = "roboweest";
 
 // list of channel names where the bot is not limited to the global 1.2 second
 // slowmode (channels it is broadcaster, moderator or VIP in)
-const modChannels = ["alazymeme", "dankfeelsbot"];
+const modChannels = ["weest", "roboweest"];
 
 // tip: use !userid <usernames...> command in the #pajlada chat to get user IDs
 // add the "protection" object to enable pajbot banphrase checking protection
@@ -57,61 +57,17 @@ function obfuscateName(str) {
 const globalLengthLimit = 480;
 
 let enabledChannels = {
-  alazymeme: {
-    id: 103973901,
-    formats: {
-      title: 'POGGERS NEW TITLE! POGGERS 👉 $VALUE$ 👉 ',
-      game: 'POGGERS NEW GAME! POGGERS 👉 $VALUE$ 👉 ',
-      live: `KKrikool GuitarTime ${obfuscateName('ALAZYMEME')} HAS GONE LIVE! KKrikool GuitarTime 👉 `,
-      offline:
-        `FeelsGoodMan TeaTime ${obfuscateName('ALAZYMEME')} HAS GONE OFFLINE! FeelsGoodMan TeaTime 👉 `
-    },
-    protection: {
-      endpoint: 'https://bot.alazymeme.com/api/v1/banphrases/test',
-      disabledCommands: ['game', 'title', 'islive']
-    },
-  },
-  pepto__bismol: {
-    id: 50495856,
-    formats: {
-      title: 'PagMan NEW TITLE! PagMan 👉 $VALUE$ 👉 ',
-      game: 'PagMan NEW GAME! PagMan 👉 $VALUE$ 👉 ',
-      live: `PagMan PEPTO__BISMOL HAS GONE LIVE! PagMan 👉 `,
-      offline:
-        `FeelsBadMan PEPTO__BISMOL HAS GONE OFFLINE! FeelsBadMan 👉 `
-    },
-    protection: {
-      endpoint: 'https://pepto.magichack.xyz/api/v1/banphrases/test',
-      disabledCommands: ['game', 'title', 'islive']
-    },
-  },
-  nymn: {
-    id: 62300805,
-    formats: {
-      title: "peepoPog NEW TITLE! peepoPog 👉 $VALUE$ 👉 ",
-      game: "peepoPog NEW GAME! peepoPog 👉 $VALUE$ 👉 ",
-      live: "peepoPog NYMN HAS GONE LIVE! peepoPog 👉 ",
-      offline:
-        "FeelsBadMan TeaTime NYMN HAS GONE OFFLINE! FeelsBadMan TeaTime 👉 ",
-    },
-    protection: {
-      endpoint: "https://nymn.pajbot.com/api/v1/banphrases/test",
-      disabledCommands: ['islive']
-    },
-  },
-  forsen: {
-    id: 22484632,
+  weest: {
+    id: 130924701,
     formats: {
       title: "PagChomp NEW TITLE! PagChomp 👉 $VALUE$ 👉 ",
       game: "PagChomp NEW GAME! PagChomp 👉 $VALUE$ 👉 ",
-      live: "KKool GuitarTime FORSEN HAS GONE LIVE! KKool GuitarTime 👉 ",
-      offline: "Okayeg TeaTime FORSEN HAS GONE OFFLINE! Okayeg TeaTime 👉 ",
+      live: "PagChomp 👉 Weest has gone live PagChomp 👉 ",
+      offline: "FeelsBadMan Weest has gone offline FeelsBadMan 👉 ",
     },
     protection: {
-      endpoint: "https://forsen.tv/api/v1/banphrases/test",
-      pajbotLinkFilter: true,
-      offlineOnly: true,
-      disabledCommands: ['islive', 'game', 'title', 'events']
+      endpoint: 'https://bot.weest.tv/api/v1/banphrases/test',
+      disabledCommands: ["game", "title", "islive"]
     },
   },
 };
