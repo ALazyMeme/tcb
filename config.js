6d624f9077c67721a7b7446afdd9e3c6413a9d47
ALazyMeme <jkeasley@icloud.com>
2021-08-01 09:33:38 +1000

    Update startup channel again

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const krakenClientId = secrets.krakenClientId;
 let administrators = ["alazymeme"];
 
 // The bot will post a "I am running"-style message to this channel on startup.
-const startupChannel = "dankfeelsbot";
+const startupChannel = "feelsokaybot";
 
 // if a channel is offline-only protected, and a change occurs, the bot prints
 // to this channel instead of the channel the change occurred in.
